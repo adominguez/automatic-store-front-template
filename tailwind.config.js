a7f6b9b8c48d50668e8db083e114ae3d25d0cf2d
adominguez <dmartin.alberto@gmail.com>
2020-12-05 10:33:55 +0100

    feat(tailwind.config): prepared for clone ith light and darken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors');
+const Color = require('color')
+const lighten = (clr, val) => Color(clr).lighten(val).rgb().string()
+const darken = (clr, val) => Color(clr).darken(val).rgb().string()
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        primary: {},
+        secondary: {},
+        backgroundSite: {},
+        textColor: {}
+      }
+    },
   },
   variants: {
     extend: {},
