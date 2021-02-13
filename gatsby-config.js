cad7cb8ebb2a5bd7515245ede8faa7dba97fa811
adominguez <dmartin.alberto@gmail.com>
2021-02-13 22:10:29 +0100

    config(gatsby-config): remove goto config in plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         output: `/sitemap.xml`,
-        exclude: [`/cookies`]
+        exclude: [`/cookies`, `goto`]
       }
     },
     {
